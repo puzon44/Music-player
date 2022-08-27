b5306e7388a0ec8b621b1f9b3c42666feb76d815
puzon44 <puzonpathak2001@gmail.com>
2022-08-27 22:57:46 +0545

    Javascript done

--- script.js
+++ script.js
@@ -1,1 +1,210 @@
-console.log('hello');
+console.log("hello");
+
+//IMG SongTitle and SingerName
+const changeImage = document.querySelector("#changeImage");
+const songTitle = document.querySelector("#songTitle");
+const singerName = document.querySelector("#singerName");
+// Music Durations
+const duration = document.querySelector(".duration");
+const currentDuration = document.querySelector(".currentDuration");
+const curntTime = document.querySelector(".curntTime");
+// Play/Pause and forward/backward
+const backward = document.querySelector("#backward");
+const playPause = document.querySelector("#playPause");
+const forward = document.querySelector("#forward");
+//Play ALl and volume section
+const playAll = document.querySelector(".playAll");
+const volumeChange = document.querySelector("#volumeChange");
+const volumeIcon = document.querySelector("#volumeIcon");
+const volumeDuration = document.querySelector("#volumeDuration");
+// hamBurger
+const hamBurger = document.querySelector("#hamBurger");
+const closeIcon = document.querySelector("#closeIcon");
+const musicPlayList = document.querySelector(".musicPlayList");
+const musicItem = document.querySelector(".musicItem");
+const playList = document.querySelector(".musicList");
+const songIndex = document.querySelector(".songIndex");
+const songName = document.querySelector(".songName");
+
+let timer = 0;
+let autoplay = 0;
+let indexMusic = 0;
+let currentTime = 0;
+let song = document.createElement("audio");
+
+//!! Event Listener
+playPause.addEventListener("click", playPauseSong);
+backward.addEventListener("click", previousSong);
+forward.addEventListener("click", forwardSong);
+playAll.addEventListener("click", autoPlay);
+volumeIcon.addEventListener("click", muteUnmute);
+volumeDuration.addEventListener("change", changeVolume);
+song.addEventListener("timeupdate", musicDuration);
+duration.addEventListener("change", sliderChange);
+hamBurger.addEventListener("click", showMusicList);
+closeIcon.addEventListener("click", hideMusicList);
+
+//!! Functions
+
+//load songs
+loadSong(indexMusic);
+
+function loadSong(indexMusic) {
+  //   clearInterval(timer);
+  //   sliderReset();
+  changeImage.src = trackList[indexMusic].image;
+  songTitle.innerText = trackList[indexMusic].name;
+  singerName.innerText = trackList[indexMusic].singer;
+  song.src = trackList[indexMusic].path;
+  song.load();
+  //   timer = setInterval(sliderUpdate, 1000);
+}
+
+function playPauseSong() {
+  {
+    if (song.paused) {
+      song.play();
+      playPause.classList.replace("fa-play", "fa-pause");
+    } else {
+      song.pause();
+      playPause.classList.replace("fa-pause", "fa-play");
+    }
+  }
+}
+
+function forwardSong() {
+  if (indexMusic < trackList.length - 1) {
+    indexMusic++;
+    loadSong(indexMusic);
+    playPauseSong();
+  } else {
+    indexMusic = 0;
+    loadSong(indexMusic);
+    playPauseSong();
+  }
+}
+
+function previousSong() {
+  if (indexMusic > 0) {
+    indexMusic--;
+    loadSong(indexMusic);
+    playPauseSong();
+  } else {
+    indexMusic = trackList.length - 1;
+    loadSong(indexMusic);
+    playPauseSong();
+  }
+}
+
+function muteUnmute() {
+  song.volume = 0;
+  volumeChange.innerText = 0;
+  volumeDuration = 0;
+  volumeIcon.classList.replace('fa-volume-up', 'fa-volume-mute')
+}
+
+function changeVolume() {
+  volumeChange.value = volumeDuration.value;
+  console.log(volumeChange.value);
+  volumeChange.innerText = volumeChange.value;
+  song.volume = volumeDuration.value / 100;
+}
+
+function autoPlay() {
+  if (autoplay === 0) {
+    autoplay = 1;
+    playAll.style.background = "#b39bba";
+  } else {
+    autoplay = 0;
+    playAll.style.background = "white";
+  }
+}
+
+function musicDuration() {
+  if (song.duration) {
+    let currentMinutes = Math.floor(song.currentTime / 60);
+    let currentSeconds = Math.floor(song.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(song.duration / 60);
+    let durationSeconds = Math.floor(song.duration - durationMinutes * 60);
+
+    if (durationSeconds < 10) {
+      durationSeconds = "0" + durationSeconds;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = "0" + durationMinutes;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    if (currentSeconds < 10) {
+      currentSeconds = "0" + currentSeconds;
+    }
+    console.log("hello mf");
+
+    curntTime.innerHTML = `${currentMinutes}:${currentSeconds}`;
+    currentDuration.innerHTML = `${durationMinutes}:${durationSeconds}`;
+  } else {
+    curntTime.innerHTML = "00" + ":" + "00";
+    currentDuration.innerHTML = "00" + ":" + "00";
+  }
+
+  // Song Duration part
+  progress = parseInt((song.currentTime / song.duration) * 100);
+  duration.value = progress;
+  // Play another song after one ends
+  if (song.ended) {
+    if (autoplay == 1 && indexMusic < trackList.length - 1) {
+      indexMusic++;
+      loadSong(indexMusic);
+      playPauseSong();
+    } else if (autoplay == 1 && indexMusic == trackList.length - 1) {
+      indexMusic = 0;
+      loadSong(indexMusic);
+      playPauseSong();
+    }
+  }
+}
+
+function sliderChange() {
+  song.currentTime = (duration.value * song.duration) / 100;
+}
+// Hamburger showing music list
+
+function showMusicList() {
+  musicPlayList.style.transform = "translateX(0)";
+}
+function hideMusicList() {
+  musicPlayList.style.transform = "translateX(-100%)";
+}
+
+// Display track in Hamburger
+
+let count = 1;
+function displaySong() {
+  for (let i = 0; i < trackList.length; i++) {
+    let div = document.createElement("div");
+    div.classList.add("musicItem");
+    div.innerHTML = `
+        <span class="songIndex">${count++}</span>
+        <p class="songName">${trackList[i].name}</p>
+    `;
+    playList.appendChild(div);
+  }
+  playSongFromList();
+}
+displaySong();
+
+function playSongFromList() {
+  playList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("songName")) {
+      const indexFromTrackList = trackList.findIndex((item, index) => {
+        if (item.name === e.target.innerText) {
+          return true;
+        }
+      });
+      loadSong(indexFromTrackList);
+      playPauseSong();
+      hideMusicList();
+    }
+  });
+}
---
+++ trackList.js
@@ -0,0 +1,46 @@
+// Music Array
+
+let trackList = [
+  {
+    name: "Faded",
+    path: "music/Faded.mp3",
+    image: "image/faded.jpg",
+    singer: "Alan Walker",
+  },
+  {
+    name: "Just the way",
+    path: "music/jtwya.m4a",
+    image: "image/2.jpg",
+    singer: "Bruno Mars",
+  },
+  {
+    name: "Dark Horse",
+    path: "music/dark horse.mp3",
+    image: "image/3.jpg",
+    singer: "Katy Perry",
+  },
+  {
+    name: "Kaise Hua",
+    path: "music/kaiseHua.mp3",
+    image: "image/4.jpg",
+    singer: "Vishal Mishra",
+  },
+  {
+    name: "Leave out",
+    path: "music/leave out.mp3",
+    image: "image/5.jpg",
+    singer: "Linkin Park",
+  },
+  {
+    name: "My life",
+    path: "music/My life.mp3",
+    image: "image/6.jpg",
+    singer: "Alok",
+  },
+  {
+    name: "Not afraid",
+    path: "music/not afraid.mp3",
+    image: "image/7.jpg",
+    singer: "Eminem",
+  },
+];
